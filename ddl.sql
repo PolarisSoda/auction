04069d30d2b41e02f29fa826b5901c7401336e3e
PolarisSoda <urin187@gmail.com>
2024-04-28 14:59:33 +0900

    hello

--- ddl.sql
+++ ddl.sql
@@ -5,10 +5,9 @@ bid  : item_ID, bid_price, bidder, date_posted, bid_closing_date
 billing : sold item_ID, purchase date, seller_ID, buyer_ID, amount due buyers need to pay, amount of money sellers need to get paid.
 i think billing is optional......
 */
-CREATE TABLE user (
+CREATE TABLE user(
     user_id varchar(32),
     password varchar(32),
     money int,
     primary key (user_id)
 );
-
