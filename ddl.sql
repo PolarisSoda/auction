c6591c9c7c5864c326c9b6f9b8f4ab0453b66e6c
PolarisSoda <urin187@gmail.com>
2024-04-28 15:01:01 +0900

    hello

--- ddl.sql
+++ ddl.sql
@@ -1,4 +1,11 @@
-CREATE TABLE user(
+/*
+user : user_ID,pwd,monee
+item : item_ID, category, description, condition, seller ID, buy-it-now-price, date posted, status(selled,selling,expired)
+bid  : item_ID, bid_price, bidder, date_posted, bid_closing_date
+billing : sold item_ID, purchase date, seller_ID, buyer_ID, amount due buyers need to pay, amount of money sellers need to get paid.
+i think billing is optional......
+*/
+CREATE TABLE user_info (
     user_id varchar(32),
     password varchar(32),
     money int,
