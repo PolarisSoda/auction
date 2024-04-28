1a2047aa6c947e59ac01c763fedc7d02ac604a51
PolarisSoda <urin187@gmail.com>
2024-04-28 13:53:55 +0900

    hello

--- Auction.java
+++ Auction.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.DriverManager;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
