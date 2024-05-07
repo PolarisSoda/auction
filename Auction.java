1c93193408f98ddc700f40f35b376eeceb11aa54
PolarisSoda <urin187@gmail.com>
2024-05-07 21:41:53 +0900

    Updated Something

--- Auction.java
+++ Auction.java
@@ -466,7 +466,7 @@ public class Auction {
 				if(arr[3].equals(username)) continue; //현재 user가 올린 item인가?
 				if(rset.getTimestamp(8).after(Timestamp.valueOf(now_time))) continue; //아 이미 끝나셨어?
 				System.out.printf("%s | %-16s | %-16s | %-16s | %-12s | %-12s | %-16s | %-12s | %s\n",arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8]);
-				items.add(arr[0]); binps.add(arr[4]); hbids.add(arr[6]);
+				items.add(arr[0]); binps.add(arr[4]); hbids.add(arr[5]);
 			}
 			pstmt.close();
 		} catch(SQLException e) {
