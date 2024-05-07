3a58cb8f5b1286ebf300d91dac287933569c4f71
PolarisSoda <urin187@gmail.com>
2024-05-07 12:32:27 +0900

    Updated Details

--- Auction.java
+++ Auction.java
@@ -446,9 +446,9 @@ public class Auction {
 				String arr[] = new String[9];
 				arr[0] = rset.getString(1); //item_id
 				arr[1] = rset.getString(5); //item_description
-				arr[2] = rset.getString(4); //item_condition
-				arr[3] = rset.getString(2); //item_seller
-				arr[4] = rset.getString(6); //item_bin_price
+				//arr[2] = rset.getString(4); //item_condition
+				//arr[3] = rset.getString(2); //item_seller
+				//arr[4] = rset.getString(6); //item_bin_price
 				//arr[5] = rset.getString(12) == null ? "-" : rset.getString(12); //item_current_bid
 				//arr[6] = rset.getString(10) == null ? "-" : rset.getString(10); //highest_bidder
 				arr[7] = Long.toString(Timestamp.valueOf(now_time).getTime() - rset.getTimestamp(8).getTime()); //time_left
